0d20d3eca4774b8d73d0a34382a10a90795a320d
daonguyendev <daonguyen.dev@gmail.com>
2023-11-17 20:08:02 +0700

    Build lesson 06 react client app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import User01 from "./components/User01";
+import User02 from "./components/User02";
+
+function App() {
+  return (<>
+    <User01/>
+    {/* <User02/> */}
+  </>);
+}
+
+export default App;
---
+++ src/components/User02.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function User02() {
+  return (
+    <div>User02</div>
+  )
+}
+
+export default User02
